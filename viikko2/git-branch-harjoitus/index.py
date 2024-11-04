c76e7d1d508b6ab91e0810c83f13cbd50eb12f60
Elias Eskelinen <git@eliaseskelinen.fi>
2024-11-04 13:55:19 +0200

    w2e7: add more changes to main

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,12 +1,14 @@
+# tehdään alussa importit
+
 from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma") # muutos mainissa
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
 
-logger("lopetetaan")
+logger("lopetetaan ohjelma")
