2fa7c52436381e53f359347d3488534d216faecc
Elias Eskelinen <git@eliaseskelinen.fi>
2024-11-04 13:57:29 +0200

    w2e7: cause a conflict

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,12 +4,12 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
+print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
 
 logger("lopetetaan ohjelma")
-print("goodbye!") # lisäys bugikorjaus-branchissa
+print("goodbye!")
