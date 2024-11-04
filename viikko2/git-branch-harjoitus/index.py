514ac2d46ce6978055d0dad1a045d402695a927a
Elias Eskelinen <git@eliaseskelinen.fi>
2024-11-04 13:54:36 +0200

    w2e7: add "bugfix"

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -11,4 +11,5 @@ y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
 
-logger("lopetetaan")
+logger("lopetetaan ohjelma")
+print("goodbye!") # lisäys bugikorjaus-branchissa
