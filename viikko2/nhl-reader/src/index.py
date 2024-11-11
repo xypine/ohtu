d62b427fcfbb16e0fe9f560f3954e5bdc3558b5c
Elias Eskelinen <git@eliaseskelinen.fi>
2024-11-11 23:48:45 +0200

    w2e12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,25 +1,13 @@
-import requests
-from player import Player
+from player import PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    # print("JSON-muotoinen vastaus:")
-    # print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-
-    players_fin = filter(lambda p: p.nationality == "FIN", players)
-    players_fin = sorted(players_fin, key=lambda l: -l.points)
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     print("Players from FIN\n")
-    for player in players_fin:
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,5 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.id = dict['id']
@@ -11,3 +13,26 @@ class Player:
     
     def __str__(self):
         return f"{self.name:20} {self.team}  {self.goals:2} + {self.assists:2} = {self.points}"
+
+class PlayerReader:
+    def __init__(self, url):
+        response = requests.get(url).json()
+
+        # print("JSON-muotoinen vastaus:")
+        # print(response)
+
+        players = []
+
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        self.players = players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players_nat = filter(lambda p: p.nationality == nationality, self.reader.players)
+        players_nat = sorted(players_nat, key=lambda l: -l.points)
+        return players_nat
