37945dbfd25a4f57edb96ba8bde326a29b6a2986
Elias Eskelinen <git@eliaseskelinen.fi>
2024-11-24 19:52:47 +0200

    w4e2: Add tests relating to kassapaate.lataa

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_ladataan_rahaa_jos_summa_positiivinen(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, 4)
+
+        maksukortti_mock.lataa.assert_called_with(4)
+
+    def test_kortille_ei_ladata_rahaa_jos_summa_negatiivinen(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, -4)
+
+        maksukortti_mock.lataa.assert_not_called()
