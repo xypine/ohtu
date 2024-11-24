94551e5217b9b52ba8adb2312c73ab9339eeada1
Elias Eskelinen <git@eliaseskelinen.fi>
2024-11-24 19:33:51 +0200

    w4e2: fix kassapääte

--- viikko4/maksukortti-mock/src/kassapaate.py
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -1,3 +1,6 @@
+from maksukortti import Maksukortti
+
+
 HINTA = 5
 
 
@@ -5,9 +8,10 @@ class Kassapaate:
     def __init__(self):
         self.__myytyja_lounaita = 0
 
-    def lataa(self, kortti, summa):
+    def lataa(self, kortti: Maksukortti, summa: int):
         kortti.lataa(summa)
 
-    def osta_lounas(self, kortti):
-        kortti.osta(HINTA)
-        self.__myytyja_lounaita = self.__myytyja_lounaita + 1
+    def osta_lounas(self, kortti: Maksukortti):
+        if kortti.saldo() >= HINTA:
+            kortti.osta(HINTA)
+            self.__myytyja_lounaita = self.__myytyja_lounaita + 1
