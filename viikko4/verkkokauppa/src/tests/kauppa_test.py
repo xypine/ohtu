cb90058a16f2aca8555876ca32471863c2869ff6
Elias Eskelinen <elias.eskelinen@pm.me>
2024-11-25 23:01:40 +0200

    w4e4: get coverage to 100%

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -113,3 +113,12 @@ class TestKauppa(unittest.TestCase):
         self.kauppa.lisaa_koriin(1)
         self.kauppa.tilimaksu("pekka", "09876")
         self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 2)
+
+    def test_poisto_korista(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.poista_korista(1)
+
+        self.kauppa.tilimaksu("pekka", "98760")
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "98760", self.kauppa._kaupan_tili, 10)
