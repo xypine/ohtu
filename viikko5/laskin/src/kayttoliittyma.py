a7981723107686f82c38cdb2332ab02401f162a3
Elias Eskelinen <git@eliaseskelinen.fi>
2024-12-02 23:38:40 +0200

    w5e5,w5e6: complete

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from tkinter import ttk, constants, StringVar
 
+from sovelluslogiikka import Sovelluslogiikka
+
 
 class Komento(Enum):
     SUMMA = 1
@@ -8,11 +10,68 @@ class Komento(Enum):
     NOLLAUS = 3
     KUMOA = 4
 
+class Summa:
+    def __init__(self, sovelluslogiikka: Sovelluslogiikka, hae_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._hae_syote = hae_syote
+
+    def suorita(self):
+        arvo = self._hae_syote()
+        self._sovelluslogiikka.plus(arvo)
+
+
+class Erotus:
+    def __init__(self, sovelluslogiikka: Sovelluslogiikka, hae_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._hae_syote = hae_syote
+
+    def suorita(self):
+        arvo = self._hae_syote()
+        self._sovelluslogiikka.miinus(arvo)
+
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka: Sovelluslogiikka, hae_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._hae_syote = hae_syote
+
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+
+class Kumoa:
+    def __init__(self, sovelluslogiikka: Sovelluslogiikka, hae_aikaisempi_arvo):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._hae_aikaisempi_arvo = hae_aikaisempi_arvo
+
+    def suorita(self):
+        aikaisempi = self._hae_aikaisempi_arvo()
+        if aikaisempi is not None:
+            self._sovelluslogiikka.aseta_arvo(aikaisempi)
 
 class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+        self._komennot = {
+            Komento.SUMMA: Summa(sovelluslogiikka, self._hae_syote),
+            Komento.EROTUS: Erotus(sovelluslogiikka, self._hae_syote),
+            Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._hae_syote),
+            Komento.KUMOA: Kumoa(sovelluslogiikka, self._hae_aikaisempi_arvo),
+        }
+        self._historia = []
+
+    def _hae_syote(self):
+        try:
+            arvo = int(self._syote_kentta.get())
+            return arvo
+        except Exception:
+            pass
+
+    def _hae_aikaisempi_arvo(self):
+        if len(self._historia) > 0:
+            return self._historia.pop()
+        return None
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -55,21 +114,11 @@ class Kayttoliittyma:
         self._kumoa_painike.grid(row=2, column=3)
 
     def _suorita_komento(self, komento):
-        arvo = 0
+        if komento != Komento.KUMOA:
+            self._historia.append(self._sovelluslogiikka.arvo())
 
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
 
         self._kumoa_painike["state"] = constants.NORMAL
 
@@ -77,6 +126,11 @@ class Kayttoliittyma:
             self._nollaus_painike["state"] = constants.DISABLED
         else:
             self._nollaus_painike["state"] = constants.NORMAL
+        
+        if len(self._historia) > 0:
+            self._kumoa_painike["state"] = constants.NORMAL
+        else:
+            self._kumoa_painike["state"] = constants.DISABLED
 
         self._syote_kentta.delete(0, constants.END)
         self._arvo_var.set(self._sovelluslogiikka.arvo())
