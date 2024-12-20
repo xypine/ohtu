4c168310e422c30faa63add61e725e5c35d8d7ef
Elias Eskelinen <git@eliaseskelinen.fi>
2024-12-20 02:13:31 +0200

    w7e3: init

---
+++ viikko7/kivi-paperi-sakset/flake.nix
@@ -0,0 +1,69 @@
+# Adapted from https://github.com/nix-community/poetry2nix/blob/master/templates/app/flake.nix
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        kivi-paperi-sakset = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
+          projectDir = self;
+          overrides = poetry2nix.overrides.withDefaults (final: super:
+            lib.mapAttrs
+              (attr: systems: super.${attr}.overridePythonAttrs
+                (old: {
+                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
+                }))
+              {
+                # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
+                # package = [ "setuptools" ];
+              }
+          );
+          python = pkgs.python3Full; # Needed for tkinter
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            poetry2nix.overlays.default
+            (final: _: {
+              kivi-paperi-sakset = final.callPackage kivi-paperi-sakset { };
+            })
+          ];
+        };
+      in
+      {
+        packages.default = pkgs.kivi-paperi-sakset;
+        devShells = {
+          # Shell for app dependencies.
+          #
+          #     nix develop
+          #
+          # Use this shell for developing your app.
+          default = pkgs.mkShell {
+            inputsFrom = [ pkgs.kivi-paperi-sakset ];
+            packages = [  ];
+          };
+
+          # Shell for poetry.
+          #
+          #     nix develop .#poetry
+          #
+          # Use this shell for changes to pyproject.toml and poetry.lock.
+          poetry = pkgs.mkShell {
+            packages = [ pkgs.poetry ];
+          };
+        };
+        legacyPackages = pkgs;
+      }
+    );
+}
